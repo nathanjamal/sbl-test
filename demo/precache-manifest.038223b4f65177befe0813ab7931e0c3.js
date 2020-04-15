f1eebe8b11f1968ea9b9cb7e6a2a333b29990832
nathan <nathanjamal@ymail.com>
2020-04-16 00:57:08 +0100

    added js folder

---
+++ demo/precache-manifest.038223b4f65177befe0813ab7931e0c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7eb01e61b25f44c35f9026c692d1b6da",
+    "url": "https://nathanjamal.github.io/sbl-test/demo/index.html"
+  },
+  {
+    "revision": "64149957e6e9df82a43c",
+    "url": "https://nathanjamal.github.io/sbl-test/demo/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "12e25df437125485be3d",
+    "url": "https://nathanjamal.github.io/sbl-test/demo/static/js/2.b3851b52.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "https://nathanjamal.github.io/sbl-test/demo/static/js/2.b3851b52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64149957e6e9df82a43c",
+    "url": "https://nathanjamal.github.io/sbl-test/demo/static/js/main.6b51f663.chunk.js"
+  },
+  {
+    "revision": "a6bad08bcc4ce4b9849b",
+    "url": "https://nathanjamal.github.io/sbl-test/demo/static/js/runtime-main.c7d9b3e9.js"
+  }
+]);
