8852a14dd6d64d1f31c18dd9f5028e7da2fce295
nathan <nathanjamal@ymail.com>
2020-04-16 00:34:09 +0100

    added site folder

---
+++ site/precache-manifest.5a2fe9adc011833b7d12007cdddd4bca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eda98f61cb0a8bdfe9143ebe40e40435",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e6e5ad148410dc640ad2",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "73575159780fd663a589",
+    "url": "/static/js/2.637ef7dc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.637ef7dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6e5ad148410dc640ad2",
+    "url": "/static/js/main.ad9335d2.chunk.js"
+  },
+  {
+    "revision": "40fed8dfce21756b31d1",
+    "url": "/static/js/runtime-main.756ec596.js"
+  }
+]);
